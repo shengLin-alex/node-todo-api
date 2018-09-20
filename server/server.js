d0a4d5bfc4ae01e1123a7e9d561c715fa50b56e1
DESKTOP-STF9L29\Sheng-Wei Lin <a182820998@gmail.com>
2018-09-20 16:50:51 +0800

    add patch api

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 let express  = require('express');
 let bodyParser = require('body-parser');
+let _ = require('lodash');
 
 let { mongoose } = require('./db/mongoose');
 let { ObjectId } = require('mongodb');
@@ -67,6 +68,34 @@ app.delete('/todos/:id', (req, res) => {
         res.send({todo});
     }).catch((err) => {
         res.status(400).send(err);
+    });
+});
+
+// PATCH /todos/:id
+app.patch('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectId.isValid(req.params.id)) {
+        return res.status(404).send({});
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    }
+    else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+        if (!todo) {
+            return res.status(404).send({});
+        }
+
+        res.send(todo);
+    }).catch((e) => {
+        res.status(400).send(e);
     });
 });
 
